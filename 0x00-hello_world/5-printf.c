1b1fb782041f9d032894f2a69db481efb76071d2
aust-ink <austin.chamberlain5@gmail.com>
2022-03-10 15:24:47 -0800

    printf file

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main- outputs a string
+ * Return 0; returned to exit program
+ * printf- displays output
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art.\n");
+	return (0);
+}
