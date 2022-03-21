a5b10c264261b26de064bce73717114b3e57a17d
aust-ink <austin.chamberlain5@gmail.com>
2022-03-21 14:04:41 -0700

    function string

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,14 +1,19 @@
-#include "holberton.h"
+#include "main.h"
+
 /**
- * _puts - prints a string, followed by a new line.
- * @str: input string to print.
+ * _puts - prints a string followed by a new line..
+ *
+ *@str: the string to print.
+ *
  */
+
 void _puts(char *str)
 {
-	while (*str != '\0')
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
 	{
-		_putchar(*str);
-		str++;
+		_putchar(str[i]);
 	}
 	_putchar('\n');
 }
